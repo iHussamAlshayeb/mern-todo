7cf8e3519b1a3401c090869fa016b4d396f32a41
Hussam Alshayeb <hussamalshayeb01@gmail.com>
2025-10-16 23:48:36 +0300

    Update todo.js

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,1 +1,9 @@
 import express from "express";
+const router = express.Router();
+
+router.post("/api/todos", (req, res) => {});
+router.get("/api/todos", (req, res) => {});
+router.put("/api/todos/:id", (req, res) => {});
+router.delete("/api/todos/:id", (req, res) => {});
+
+export default router;
